a47efdc2117f9b416b26f932a10e670460e1b9e4
Daniel <81906554+danield208@users.noreply.github.com>
2023-05-06 17:14:26 +0200

    added cv function and change picture

--- src/app/mainpage/personal-info/personal-info.component.ts
+++ src/app/mainpage/personal-info/personal-info.component.ts
@@ -1,16 +1,19 @@
 import { Component } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
+import {DataService} from "../../_service/data.service";
+import {Data} from "@angular/router";
 
 @Component({
 	selector: "app-personal-info",
 	templateUrl: "./personal-info.component.html",
 	styleUrls: ["./personal-info.component.scss"],
+	providers: [DataService],
 })
 export class PersonalInfoComponent {
 	personalData: Array<any> = [];
 	arrayLenght!: number;
 
-	constructor(public translate: TranslateService) {
+	constructor(public translate: TranslateService, private data: DataService) {
 		this.getPersonalData();
 	}
 
@@ -23,5 +26,12 @@ export class PersonalInfoComponent {
 				});
 			});
 		this.arrayLenght = this.personalData.length;
+	}
+
+	showCV(url: string) {
+		this.data.downloadPDF(url).subscribe(res => {
+			const fileURL = URL.createObjectURL(res);
+			window.open(fileURL, '_blank');
+		});
 	}
 }
